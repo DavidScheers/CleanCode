832ec1d6a371909095c39e081b092dd2fa31bdab
DavidScheers <david.scheers@gmail.com>
2017-02-23 10:10:46 +0100

    Added Groceries class

---
+++ src/main/java/Groceries.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by davids on 23/02/2017.
+ */
+public class Groceries {
+
+    private Map<String, Integer> shoppingCart;
+
+    public Groceries() {
+        this.shoppingCart = new HashMap<>();
+    }
+
+    public Map<String, Integer> getShoppingCart() {
+        return shoppingCart;
+    }
+
+    public void buyItem(String item, int quantity) {
+        if (shoppingCart.containsKey(item)) {
+            
+        }
+        shoppingCart.put(item, quantity);
+    }
+}
